982c43db885b1be4d0369561fc1ad2e6fc0a6892
Gourav7777 <360inside360@gmail.com>
2023-04-25 10:18:14 +0530

    lopop

--- mock-6/my-app/src/Components/Todos.jsx
+++ mock-6/my-app/src/Components/Todos.jsx
@@ -1,13 +1,107 @@
-import React from 'react'
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    Button,
+    FormLabel,
+    FormControl,
+    Input,
+  } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { chngstatusTodo, del, ed } from '../Redux/action'
 
 const Todos = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const initialRef = React.useRef(null)
+  const finalRef = React.useRef(null)
+
+    const todos = useSelector((store)=>store.todos)
+    const dispatch = useDispatch()
+const [idd,setIdd] = useState(0)
+ 
+    const handleChange=(e,id)=>{
+
+
+        dispatch(chngstatusTodo(id))
+        
+    }
+
+
+
+    const handleDelete=(id)=>{
+
+        dispatch(del(id))
+    }
+
+    const handleEdit=(id)=>{
+  setIdd(id)
+        onOpen()
+    }
+
+    const handleActualEdit=()=>{
+         let x = initialRef.current.value
+        dispatch(ed({idd,x}))
+        console.log(idd,initialRef.current.value)
+        onClose()
+setIdd(0)
+        initialRef.current.value = null
+    }
   return (
+
+    <>
+ 
     <div>
         
-        Todos
+        {
+            todos?todos.map((el)=>{
+                return <div>
+      <h1>{el.task}</h1>
+        <input type="checkbox" checked={el.status} onChange={(e)=>handleChange(e,el.id)}/>
+        <Button onClick={()=>handleEdit(el.id)}>Edit</Button>
+         
+        
+        <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create your account</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Edit Task</FormLabel>
+              <Input ref={initialRef} placeholder='First name' />
+            </FormControl>
 
+            
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3}  onClick={()=>handleActualEdit()} >
+              Edit from here
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+            <Button onClick={()=>handleDelete(el.id)}>DELETE</Button>
+                  </div>
+              }):<div>No Todos</div>
+          }
 
     </div>
+        </>
   )
 }
 
--- mock-6/my-app/src/Redux/action.js
+++ mock-6/my-app/src/Redux/action.js
@@ -1,4 +1,4 @@
-import { ADD_TODO } from "./actiontype"
+import { ADD_TODO, CHANGE_STATUS, DEL_TODO, EDIT_TODO } from "./actiontype"
 
 export const saveTodo = (payload)=>{
 
@@ -7,11 +7,64 @@ return {
     payload
 }
 
+
+
 }
 
+
+export const changeStatus=(payload)=>{
+    return {
+        type:CHANGE_STATUS,
+        payload
+    }
+}
+
+
+
+export const deltodo=(payload)=>{
+    return{
+        type:DEL_TODO,
+        payload
+    }
+}
+
+
+export const edit =(payload)=>{
+// console.log(payload)
+     return {
+        type:EDIT_TODO,
+        payload
+     }
+}
+
+
+
+
+
+
 export const addTodo =(payload)=>(dispatch)=>{
-    
+  
    dispatch(saveTodo(payload))
 
+
+}
+
+export const chngstatusTodo =(payload)=>(dispatch)=>{
+    
+   dispatch(changeStatus(payload))
+
+
+}
+
+export const del=(payload)=>(dispatch)=>{
+
+    dispatch(deltodo(payload))
+}
+
+
+
+export const ed = (payload)=>(dispatch)=>{
+
+ dispatch(edit(payload))
 
 }
